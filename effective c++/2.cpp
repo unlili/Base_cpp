e581dd5b6f96da7b9a6fcfde663ec09a2fd1b757
Zihang <45307866+unlili@users.noreply.github.com>
2020-11-16 15:55:13 +0800

    使用枚举const inline 替换宏

--- effective c++/2.cpp
+++ effective c++/2.cpp
@@ -6,10 +6,21 @@
   类内的宏定义是没有作用域的，使用enum可以防止取地址操作
   enum { AAA }  ---> 
  
-  inline 函数
+  inline 函数 替换宏函数
 
 #endif
 
 
+#define MAX(a,b) \
+  do{\
+    f( (a)>(b):(a)?(b) )\
+  } while (0)
+//---->
+
+template<typename T>
+inline T max(T a,T b)
+{
+    return f(a>b ? a : b)  
+}
 
 
